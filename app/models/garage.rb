066d7b4d5009ac581afd9254b219b8ab3ef3e928
Pedro Ivo <pedroivo@x1s.eti.br>
2014-01-05 14:35:14 +0000

    fix uploaders

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -6,7 +6,7 @@ class Garage < ActiveRecord::Base
 
   has_attached_file :avatar, 
     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-    :default_url => "/images/:style/missing.png"
+    :default_url => "/images/:style/missing.png",
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename"
 
